91dd486d6d14800d124cf54f750b228abe95d328
Samuel Becker <samuel.becker1801@web.de>
2020-10-12 21:27:59 +0200

    xxx

--- EIA2/Cows/js/Cows.js
+++ EIA2/Cows/js/Cows.js
@@ -1,18 +1,19 @@
-namespace Cows {
-    let nums: number[] = [2, 6, 5];
-    let results: string[] = [];
-    for (let i: number = 0; i < nums.length; i++) {
-        let result: string = createCall("m", nums[i]);
+var Cows;
+(function (Cows) {
+    let nums = [2, 6, 5];
+    let results = [];
+    for (let i = 0; i < nums.length; i++) {
+        let result = createCall("m", nums[i]);
         results.push(result);
     }
     console.log(results);
-
-    function createCall(_start: string, _length: number): string {
-        for (let k: number = _length; k > 0; k--) {
+    function createCall(_start, _length) {
+        for (let k = _length; k > 0; k--) {
             if (k == 1 || k == _length / 2)
                 _start += "h";
             _start += "u";
         }
         return _start;
     }
-}
+})(Cows || (Cows = {}));
+//# sourceMappingURL=Cows.js.map
